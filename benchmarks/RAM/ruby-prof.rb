9dc3bd65dec1befc9972c360ed779f02054afe87
VidaspVii <vidasp@mail.ru>
2019-08-05 01:19:55 +0300

    Add profiles from ruby-prof

--- benchmarks/RAM/ruby-prof.rb
+++ benchmarks/RAM/ruby-prof.rb
@@ -5,8 +5,17 @@ RubyProf.measure_mode = RubyProf::MEMORY
 
 result = RubyProf.profile do
   GC.disable
-  work('../../data_large.txt')
+  work('../../data-500.txt')
 end
 
-printer = RubyProf::FlatPrinter.new(result)
-printer.print(File.open("reports/flat.txt", "w+"))
+# printer = RubyProf::FlatPrinter.new(result)
+# printer.print(File.open("reports/flat.txt", "w+"))
+
+# printer = RubyProf::GraphPrinter.new(result)
+# printer.print(STDOUT, :min_percent => 2)
+#
+# printer = RubyProf::MultiPrinter.new(result)
+
+printer = RubyProf::CallTreePrinter(result) #dont work  undefined method `CallTreePrinter' for RubyProf:Module
+
+printer.print(:path => ".", :profile => "profile")

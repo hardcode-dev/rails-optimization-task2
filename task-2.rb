7852c7fd07e52b4a79b0500a182429b81bbbd1cf
VidaspVii <vidasp@mail.ru>
2019-08-08 23:51:59 +0300

    Update task. Reduce count retained

--- task-2.rb
+++ task-2.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'set'
 require 'json'
 
@@ -6,16 +8,15 @@ SPLIT = ','
 MIN = 'min.'
 WS = ' '
 COMMA = ', '
-BROWSERS = %w[CHROME INTERNET\ EXPLORER FIREFOX SAFARI].freeze
+BROWSERS = %w[CHROME INTERNET\ EXPLORER].freeze
 
 def work(file)
   filer   = File.new('result.json', 'w')
   @report = { totalUsers: 0, uniqueBrowsersCount: 0, totalSessions: 0, allBrowsers: Set.new, usersStats: {} }
-  @user   = ''
 
   File.foreach(file) do |line|
     is_user = line.include?(USER)
-    @user = is_user ? line.split(SPLIT)[2..3].join(WS) : @user
+    @user = is_user ? user_name(line) : @user
     make_report(line, @user, is_user)
   end
 
@@ -26,6 +27,11 @@ end
 
 private
 
+def user_name(line)
+  n = line.split(SPLIT)
+  "#{n[2]} #{n[3]}"
+end
+
 def browser_decoration(browsers)
   browsers.sort.join(SPLIT)
 end
@@ -33,8 +39,8 @@ end
 def make_report(line, user, is_user = false)
   if is_user
     @report[:usersStats][user] = { sessionsCount: 0,
-                                   totalTime: [0, MIN],
-                                   longestSession: [0, MIN],
+                                   totalTime: 0,
+                                   longestSession: 0,
                                    browsers: [],
                                    usedIE: false,
                                    alwaysUsedChrome: true,
@@ -56,8 +62,8 @@ def make_report(line, user, is_user = false)
         @report[:usersStats][user][:usedIE] = true if @report[:usersStats][user][:usedIE] || data.include?(BROWSERS[1])
         @report[:usersStats][user][:browsers] << data
       when 5
-        @report[:usersStats][user][:totalTime][0] += data.to_i
-        @report[:usersStats][user][:longestSession][0] = data.to_i if @report[:usersStats][user][:longestSession][0] < data.to_i
+        @report[:usersStats][user][:totalTime] += data.to_i
+        @report[:usersStats][user][:longestSession] = data.to_i if @report[:usersStats][user][:longestSession] < data.to_i
       when 6
         @report[:usersStats][user][:dates] << data.chomp
       end
@@ -73,10 +79,11 @@ def prepare_report
   @report[:allBrowsers]         = browser_decoration(@report[:allBrowsers])
 
   @report[:usersStats].each_value do |user|
-    user[:totalTime]      = user[:totalTime].join(WS)
-    user[:browsers]       = user[:browsers].sort.join(COMMA)
-    user[:longestSession] = user[:longestSession].join(WS)
-    user[:dates]          = user[:dates].sort.reverse
+    user[:totalTime]      = "#{user[:totalTime]} #{MIN}"
+    user[:browsers].sort!
+    user[:browsers]       = user[:browsers].join(COMMA)
+    user[:longestSession] = "#{user[:longestSession]} #{MIN}"
+    user[:dates]          = user[:dates].sort {|a,b| b<=>a}
   end
 end
 

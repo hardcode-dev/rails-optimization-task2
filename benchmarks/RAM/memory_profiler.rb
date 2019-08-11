841c0ac02be6723960f7db1f2efc87d14ce227d0
VidaspVii <vidasp@mail.ru>
2019-08-11 03:31:52 +0300

    Update task-2 16Mb

--- benchmarks/RAM/memory_profiler.rb
+++ benchmarks/RAM/memory_profiler.rb
@@ -2,5 +2,5 @@ require 'memory_profiler'
 require_relative '../../task-2'
 
 MemoryProfiler.report do
-  work('../../data-500.txt')
+  work('../../data_large.txt')
 end.pretty_print(to_file: 'reports/memory_profiler.txt', scale_bytes: true)
--- task-2.rb
+++ task-2.rb
@@ -9,41 +9,75 @@ MIN = 'min.'
 WS = ' '
 COMMA = ', '
 BROWSERS = %w[CHROME INTERNET\ EXPLORER].freeze
+TMP_FILE = '.result.tmp'
 
 def work(file)
-  result_file = File.new('result.json', 'w')
-  tmp_file   = File.new('.result.tmp', 'a:UTF-8')
-  report = { 'totalUsers' => 0, 'uniqueBrowsersCount' => 0, 'totalSessions' => 0, 'allBrowsers' => Set.new, 'usersStats' => {} }
+  File.delete('result.json') if File.exist?('result.json')
 
+  result_file = File.new("result.json", 'a:UTF-8')
+  tmp_file    = File.new(TMP_FILE, 'a:UTF-8')
+  report      = { 'totalUsers' => 0, 'uniqueBrowsersCount' => 0,
+                  'totalSessions' => 0, 'allBrowsers' => Set.new, 'usersStats' => {} }
+
+  # Скрапим и агрегируем данные
+  scraping_data_file(file, tmp_file, report)
+  aggregation_browser_data(report)
+
+  # Если тут не закрыть tmp файл то читать будет не чего.
+  tmp_file.close
+
+  # Запись в файл
+  report = "#{Oj.dump(report)}".delete_suffix("}}")
+  result_file.write(report)
+  need_name(result_file)
+
+  # Подчищаем за собой мусор
+  result_file.close
+  File.delete(TMP_FILE)
+end
+
+private
+
+def need_name(result_file)
+  File.foreach(TMP_FILE) do |line|
+    str = line.to_s.delete_suffix("\n")
+    result_file.write(str)
+  end
+
+  result_file.write("}}\n")
+end
+
+def scraping_data_file(file, tmp_file, report)
   last_string = `wc -l #{file}`.split(WS)[0].to_i
   current_string = 0
 
   File.foreach(file) do |line|
     current_string += 1
+
     is_user = line.include?(USER)
     @user  = is_user ? user_name(line) : @user
     make_report(line, @user, is_user, report)
 
     save_interim_report(report, tmp_file, last_string, current_string)
   end
-
-  prepare_user_report(report)
-
-  aggregation_browser_data(report)
-  result_file.write "#{Oj.dump(report)}\n"
-
-  result_file.close
-  tmp_file.close
 end
-
-private
 
 def save_interim_report(report, tmp_file, last_string, current_string)
   return unless last_string == current_string || report['usersStats'].keys.length > 1
 
+  # Берем первого пользователя.
+  # И причесываем его данные для предварительного сохранения во временный файл
   user_name   = report['usersStats'].keys[0]
+  prepare_user_report(report, user_name)
   user_report = report['usersStats'][user_name]
-  tmp_file.write("\"#{user_name}\":#{Oj.dump(user_report)} \r")
+
+  if last_string == current_string
+    tmp_file.write("\"#{user_name}\":#{Oj.dump(user_report)}\n")
+  else
+    tmp_file.write("\"#{user_name}\":#{Oj.dump(user_report)},\n")
+  end
+
+  # После записи пользователя в файл. Пользователя из хеша report надо удалить
   report['usersStats'].tap { |users| users.delete(user_name) }
 end
 
@@ -99,20 +133,20 @@ def aggregation_browser_data(report)
   report['allBrowsers']         = report['allBrowsers'].sort.join(SPLIT)
 end
 
-def prepare_user_report(report)
-  report['usersStats'].each_value do |user|
-    user['totalTime']      = "#{user['totalTime']} #{MIN}"
-    user['browsers'].sort!
-    user['browsers']       = user['browsers'].join(COMMA)
-    user['longestSession'] = "#{user['longestSession']} #{MIN}"
-    user['dates']          = user['dates'].split(WS).sort!.reverse
-  end
+def prepare_user_report(report, user)
+  user = report['usersStats'][user]
+
+  user['totalTime']      = "#{user['totalTime']} #{MIN}"
+  user['browsers'].sort!
+  user['browsers']       = user['browsers'].join(COMMA)
+  user['longestSession'] = "#{user['longestSession']} #{MIN}"
+  user['dates']          = user['dates'].split(WS).sort!.reverse
 end
 
 def print_memory_usage
   "%d MB" % (`ps -o rss= -p #{Process.pid}`.to_i / 1024)
 end
 
-work('data_large.txt')
+# work('data_large.txt')
 
-p print_memory_usage       # 959 MB
+p print_memory_usage       # 16 MB =))

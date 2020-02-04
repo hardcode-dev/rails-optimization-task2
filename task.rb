c0a71a42cf82f3fa4b36d554c668e2666b490e6d
{()}JIer <kolomoets.oleg@gmail.com>
2020-02-04 23:54:06 +0200

    Move task-1 to the repo. Remove task-2 file.

---
+++ task.rb
@@ -0,0 +1,31 @@
+require 'oj'
+require './report'
+
+Oj.mimic_JSON
+
+class Task
+  attr_reader :file_path
+
+  def initialize(file_path)
+    @file_path = file_path
+  end
+
+  def generate_report
+    report = Report.new
+    IO.foreach(file_path) do |line|
+      cols = line.split(',')
+      if cols[0] == 'user'
+        report.commit_user
+        report.add_user cols
+      else
+        report.update_session_stat(cols[3])
+        report.update_user_stat(cols)
+      end
+    end
+    report.commit_stats
+    stats = report.sessions_stats
+
+    File.write('result.json', "#{Oj.dump(stats)}\n")
+    puts "MEMORY USAGE: %d MB" % (`ps -o rss= -p #{Process.pid}`.to_i / 1024)
+  end
+end

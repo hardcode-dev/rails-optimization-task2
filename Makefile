8267def7a02847fc2eff79df8328aae641f174d6
Ilya Merkulov <imerkulov@me.com>
2020-07-25 01:16:21 +0300

    Add memory test and case-study.md

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ samples:
 	ruby benchmark/data_samples.rb
 test:
 	ruby test/task_2_test.rb
+	ruby test/memory_test.rb
 	rspec test/performance_spec.rb
 bench:
 	ruby benchmark/benchmark_realtime.rb
---
+++ test/memory_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative '../task-2'
+
+class TestMe < Minitest::Test
+  def memory_usage
+    `ps -o rss= -p #{Process.pid}`.to_i / 1024
+  end
+
+  def test_result
+    memory_usage_before= memory_usage
+    work('tmp/data_80000.txt')
+    memory_usage_after = memory_usage
+    mem_usage = memory_usage_after - memory_usage_before
+    puts "mem_usage: #{mem_usage} MB"
+    assert_operator mem_usage, :<=, 90
+  end
+end

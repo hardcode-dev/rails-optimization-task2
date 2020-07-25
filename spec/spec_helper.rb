9fc447d0982dd5bcb89d720a831fdde2c3ce6936
SupDex <symeton@gmail.com>
2020-07-25 19:37:24 +0300

    clear spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,12 +11,13 @@
 # a separate helper file that requires the additional dependencies and performs
 # the additional setup, and require it from the spec files that actually need
 # it.
-#
+
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods

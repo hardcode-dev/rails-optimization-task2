92d534e94f858fc8434625bb1b9f300ae51a0dd8
rabadan731 <rabadan731@yandex.ru>
2020-07-27 21:59:53 +0300

    STEP 4

--- task-2.rb
+++ task-2.rb
@@ -2,36 +2,7 @@
 # frozen_string_literal: true
 
 require 'json'
-require 'pry'
 require 'date'
-
-class User
-  attr_reader :attributes, :sessions
-
-  def initialize(attributes:, sessions:)
-    @attributes = attributes
-    @sessions = sessions
-  end
-end
-
-def parse_user(fields)
-  {
-    'id' => fields[1],
-    'first_name' => fields[2],
-    'last_name' => fields[3],
-    'age' => fields[4],
-  }
-end
-
-def parse_session(fields)
-  {
-    'user_id' => fields[1],
-    'session_id' => fields[2],
-    'browser' => fields[3].upcase,
-    'time' => fields[4],
-    'date' => fields[5].chomp,
-  }
-end
 
 def collect_stats_from_users(report, users_objects, &block)
   users_objects.each do |user|
@@ -41,22 +12,17 @@ def collect_stats_from_users(report, users_objects, &block)
   end
 end
 
-def report_write_user(report_file, user, user_sessions)
-  # Статистика по пользователям
-  user_key = "#{user['first_name']} #{user['last_name']}"
-  times = user_sessions.map {|s| s['time'].to_i }
-  browser = user_sessions.map {|s| s['browser']}
-
+def report_write_user(report_file, user)
   report = {}
-  report['sessionsCount'] = user_sessions.count
-  report['totalTime'] = "#{times.sum} min."
-  report['longestSession'] = "#{times.max} min."
-  report['browsers'] = browser.sort.join(', ')
-  report['usedIE'] = browser.any? { |b| b =~ /INTERNET EXPLORER/ }
-  report['alwaysUsedChrome'] = browser.all? { |b| b =~ /CHROME/ }
-  report['dates'] = user_sessions.map{|s| s['date']}.sort.reverse
+  report['sessionsCount'] = user[:times].count
+  report['totalTime'] = "#{user[:times].sum} min."
+  report['longestSession'] = "#{user[:times].max} min."
+  report['browsers'] = user[:browsers].sort.join(', ')
+  report['usedIE'] = user[:browsers].any? { |b| b =~ /INTERNET EXPLORER/ }
+  report['alwaysUsedChrome'] = user[:browsers].all? { |b| b =~ /CHROME/ }
+  report['dates'] = user[:dates].sort!.reverse!
 
-  report_file.write("\"#{user_key}\":#{report.to_json}")
+  report_file.write("\"#{user[:first_name]} #{user[:last_name]}\":#{report.to_json}")
 end
 
 def work(file_path = 'data/data.txt')
@@ -66,36 +32,45 @@ def work(file_path = 'data/data.txt')
   user = nil
   user_count = 0
   session_count = 0
-  browser = []
-  sessions = []
+  browsers = []
 
   IO.foreach(file_path) do |line|
-    cols = line.split(',')
+    cols = line.chomp.split(',')
     if cols[0] == 'user'
       unless user.nil?
-        report_write_user(report_file, user, sessions)
+        report_write_user(report_file, user)
         report_file.write(',')
       end
-      user = parse_user(cols)
-      sessions = []
+      user = {
+        first_name: cols[2],
+        last_name: cols[3],
+        times: [],
+        browsers: [],
+        dates: []
+      }
+
       user_count += 1
     else
-      session = parse_session(cols)
-      browser << session['browser']
-      sessions << session
+      browser = cols[3].upcase
+
+      user[:times] << cols[4].to_i
+      user[:dates] << cols[5]
+      user[:browsers] << browser
+
+      browsers << browser
       session_count += 1
     end
   end
 
-  report_write_user(report_file, user, sessions)
+  report_write_user(report_file, user)
 
-  browser.uniq!
-  browser.sort!
+  browsers.uniq!
+  browsers.sort!
 
   report_file.write("},\"totalUsers\":#{user_count},")
-  report_file.write("\"uniqueBrowsersCount\":#{browser.count},")
+  report_file.write("\"uniqueBrowsersCount\":#{browsers.count},")
   report_file.write("\"totalSessions\":#{session_count},")
-  report_file.write("\"allBrowsers\":\"#{browser.join(',')}\"}")
+  report_file.write("\"allBrowsers\":\"#{browsers.join(',')}\"}")
   report_file.close
   puts "MEMORY USAGE: %d MB" % (`ps -o rss= -p #{Process.pid}`.to_i / 1024)
 end
--- test/stackprof.rb
+++ test/stackprof.rb
@@ -2,7 +2,7 @@ require 'stackprof'
 require_relative '../task-2.rb'
 file_path = "#{__dir__}/../data/data1.txt"
 report_path = "#{__dir__}/../tmp/stackprof.dump"
-flamegraph_path = "#{__dir__}/../tmp/flamegraph"
+stackprof_path = "#{__dir__}/../tmp/stackprof.json"
 
 GC.disable
 
@@ -10,5 +10,8 @@ StackProf.run(mode: :object, out: report_path, raw: true) do
   work(file_path)
 end
 
-system "stackprof --flamegraph #{report_path} > #{flamegraph_path}"
+profile = StackProf.run(mode: :object, raw: true) do
+  work(file_path)
+end
 
+File.write(stackprof_path, JSON.generate(profile))

72d6358d182d36d4731e41025842cda0d98cbf9a
ifilonov <filonovia@gmail.com>
2021-04-27 23:40:36 +0500

    add rspec memory test

--- rspec_test_performance.rb
+++ rspec_test_performance.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 require 'rspec-benchmark'
-require_relative 'task-2.rb'
+require_relative 'task-2'
 
 RSpec.configure do |config|
   config.include RSpec::Benchmark::Matchers
@@ -16,16 +18,23 @@ end
 
 describe 'Performance' do
   describe 'linear work' do
-    let(:size) { 500000 }
+    let(:size) { 500_000 }
     it 'works under 5 s' do
-      expect {
+      expect do
         linear_work(size)
-      }.to perform_under(5).sec.warmup(2).times.sample(5).times
+      end.to perform_under(5).sec.warmup(2).times.sample(5).times
     end
 
-    let(:sizes) { [500000, 1000000, 1500000] }
+    let(:sizes) { [500_000, 1_000_000, 1_500_000] }
     it 'performs linear' do
-        expect { |n, _i| linear_work(n) }.to perform_linear.in_range(sizes)
+      expect { |n, _i| linear_work(n) }.to perform_linear.in_range(sizes)
+    end
+
+    let(:size_for_mem) { 50_000 }
+    it 'works under 50Mb' do
+      expect do
+        linear_work(size_for_mem)
+      end.to perform_allocation(70_000_000).bytes
     end
   end
 end

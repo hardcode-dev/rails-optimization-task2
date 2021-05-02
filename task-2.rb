5fa17a8075833a1c5160509fb5a5f28173fd4888
Konstantin Goryachev <kevinglare@gmail.com>
2021-05-02 18:22:42 +0300

    feat: optimization from first task

--- task-2.rb
+++ task-2.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'json'
+require 'oj'
 
 def parse_user(fields)
   {
@@ -11,54 +11,76 @@ end
 
 def parse_session(fields)
   {
-    'browser' => fields[3],
+    'browser' => fields[3].upcase!,
     'time' => fields[4].to_i,
     'date' => fields[5]
   }
 end
 
 def work(file_name = 'data.txt')
+  file_lines = File.read(file_name).split("\n")
+
   users = []
   sessions = []
-  report = {}
-  report['totalUsers'] = 0
-  report['totalSessions'] = 0
   unique_browsers = {}
+  sessions_by_user_id = {}
 
-  IO.foreach(file_name) do |line|
+  report = { totalUsers: 0, uniqueBrowsersCount: 0, totalSessions: 0, allBrowsers: nil, usersStats: {} }
+
+  file_lines.each do |line|
     cols = line.split(',')
-    if cols[0] == 'user'
+    case cols[0]
+    when 'user'
       users << parse_user(cols)
-      report['totalUsers'] += 1
-    else
-      cols[3].upcase
-      sessions << parse_session(cols)
-      report['totalSessions'] += 1
-      unique_browsers[cols[3]] = nil
+      report[:totalUsers] += 1
+    when 'session'
+      session = parse_session(cols)
+      sessions << session
+      browser = session['browser']
+      user_id = cols[1]
+      sessions_by_user_id[user_id] ||= []
+      sessions_by_user_id[user_id] << session
+      unique_browsers[browser] = nil
+      report[:totalSessions] += 1
     end
   end
 
-  unique_browsers = unique_browsers.keys.sort
+  unique_browsers = unique_browsers.keys.sort!
 
-  report['unique_browsersCount'] = unique_browsers.count
-  report['allBrowsers'] = unique_browsers.join(',')
-  report['usersStats'] = {}
+  report[:uniqueBrowsersCount] = unique_browsers.count
+  report[:allBrowsers] = unique_browsers.join(',')
 
+  # Статистика по пользователям
   users.each do |user|
-    user_sessions = sessions.select { |session| session['user_id'] == user['id'] }
-    user_key = "#{user['first_name']} #{user['last_name']}"
+    user_sessions = sessions_by_user_id[user['id']]
 
-    report['usersStats'][user_key] = {
-      'sessionsCount' => user_sessions.count,
-      'totalTime' => "#{user_sessions.map { |s| s['time'] }.sum} min.",
-      'longestSession' => "#{user_sessions.map { |s| s['time'] }.max} min.",
-      'browsers' => user_sessions.map { |s| s['browser'] }.join(', '),
-      'usedIE' => user_sessions.map { |s| s['browser'] }.any? { |b| b.upcase =~ /INTERNET EXPLORER/ },
-      'alwaysUsedChrome' => user_sessions.map { |s| s['browser'] }.all? { |b| b =~ /CHROME/ },
-      'dates' => user_sessions.map! { |s| s['date'] }
+    time_sum = 0
+    longest_session = 0
+    browsers = []
+    dates = []
+
+    user_sessions.each do |s|
+      time = s['time']
+      browser = s['browser']
+      time_sum += time
+      longest_session = time if longest_session < time
+      browsers << browser
+      dates << s['date']
+    end
+
+    browsers.sort!
+
+    report[:usersStats][user['user_key']] = {
+      sessionsCount: user_sessions.count,
+      totalTime: "#{time_sum} min.",
+      longestSession: "#{longest_session} min.",
+      browsers: browsers.join(', '),
+      usedIE: browsers.any? { |b| b.start_with?('I') },
+      alwaysUsedChrome: browsers.all? { |b| b.start_with?('C') },
+      dates: dates.sort!.reverse!
     }
   end
 
-  File.write('result.json', "#{report.to_json}\n")
+  File.write('result.json', "#{Oj.dump(report, mode: :compat)}\n")
   puts "MEMORY USAGE: %d MB" % (`ps -o rss= -p #{Process.pid}`.to_i / 1024)
 end

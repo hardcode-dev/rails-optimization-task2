6ff7ee61c6cdad1e6a7fd9d1bd918a9d8d49e583
otmosina <otmosina@gmail.com>
2021-05-05 13:49:48 +0800

    case #4: strip and map

--- task-2.rb
+++ task-2.rb
@@ -22,25 +22,28 @@ def parse_session(fields)
     'session_id' => fields[2],
     'browser' => fields[3].upcase,
     'time' => fields[4],
-    'date' => fields[5],
+    'date' => fields[5].strip,
   }
 end
 
 
 def add_stasts sessions
+  browsers_map = sessions.map {|s| s['browser']}
+  time_map = sessions.map {|s| s['time'].to_i}
   { 
     'sessionsCount' => sessions.count, 
   # Собираем количество времени по пользователям
-    'totalTime' => sessions.map {|s| s['time']}.map {|t| t.to_i}.sum.to_s + ' min.',
+    'totalTime' => time_map.sum.to_s + ' min.',
   # Выбираем самую длинную сессию пользователя
-    'longestSession' => sessions.map {|s| s['time']}.map {|t| t.to_i}.max.to_s + ' min.',
-  # Браузеры пользователя через запятую
-    'browsers' => sessions.map {|s| s['browser']}.map {|b| b}.sort.join(', '),
+    'longestSession' => time_map.max.to_s + ' min.',
   # Хоть раз использовал IE?
-    'usedIE' => sessions.map{|s| s['browser']}.any? { |b| b =~ /INTERNET EXPLORER/ },
-    'alwaysUsedChrome' => sessions.map{|s| s['browser']}.all? { |b| b.upcase =~ /CHROME/ },
+    'usedIE' => browsers_map.any? { |b| b =~ /INTERNET EXPLORER/ },
+    'alwaysUsedChrome' => browsers_map.all? { |b| b.upcase =~ /CHROME/ },    
+  # Браузеры пользователя через запятую
+    'browsers' => browsers_map.map! {|b| b}.sort.join(', '),
+
     # Даты сессий через запятую в обратном порядке в формате iso8601
-    'dates' => sessions.map{|s| s['date'].strip}.sort.reverse#.map{|d|d},
+    'dates' => sessions.map{|s| s['date']}.sort.reverse
   }
 
 end 

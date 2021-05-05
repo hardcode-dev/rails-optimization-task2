3d1c7fd52c51a17768b0f137a752b9ac9d1ad5a9
otmosina <otmosina@gmail.com>
2021-05-05 16:37:25 +0800

    case #6: upcase&strip with bang

--- task-2.rb
+++ task-2.rb
@@ -20,9 +20,9 @@ def parse_session(fields)
   parsed_result = {
     'user_id' => fields[1],
     'session_id' => fields[2],
-    'browser' => fields[3].upcase,
+    'browser' => fields[3].upcase!,
     'time' => fields[4],
-    'date' => fields[5].strip,
+    'date' => fields[5].strip!,
   }
 end
 

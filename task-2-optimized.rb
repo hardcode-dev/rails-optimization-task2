fc081df465edc81787f003769764f5c2ab55041f
Sergey Budaev <sergey.budaev@gmail.com>
2021-05-09 17:48:53 +0300

    iteration 12

--- task-2-optimized.rb
+++ task-2-optimized.rb
@@ -20,39 +20,41 @@ class ParserOptimized
     CACHED_DATES = { }
 
     # Fields name
-    ID = 'id'.freeze
-    FIRST_NAME = 'first_name'.freeze
-    LAST_NAME = 'last_name'.freeze
-    AGE = 'age'.freeze
-    USER_ID = 'user_id'.freeze
-    SESSION_ID = 'session_id'.freeze
-    BROWSER = 'browser'.freeze
-    TIME = 'time'.freeze
-    DATE = 'date'.freeze
+    ID = :id
+    FIRST_NAME = :first_name
+    LAST_NAME = :last_name
+    AGE = :age
+    USER_ID = :user_id
+    SESSION_ID = :session_id
+    BROWSER = :browser
+    TIME = :time
+    DATE = :date
 
     # row types
     SESSION = 'session'.freeze
     USER = 'user'.freeze
 
+    DATE_FORMAT = '%Y-%m-%d'
+
     def parse_date(date)
-      CACHED_DATES[date] ||= Date.strptime(date, '%Y-%m-%d').iso8601
+      CACHED_DATES[date] ||= Date.strptime(date, DATE_FORMAT).iso8601
     end
 
     def parse_user(line)
       _, id, first_name, last_name, age = line.split(SEPARATOR)
       {
-        ID => id,
+        ID => id.to_i,
         FIRST_NAME => first_name,
         LAST_NAME => last_name,
-        AGE => age,
+        AGE => age.to_i,
       }
     end
 
     def parse_session(line)
       _, user_id, session_id, browser, time, date = line.split(SEPARATOR)
       {
-        USER_ID => user_id,
-        SESSION_ID => session_id,
+        USER_ID => user_id.to_i,
+        SESSION_ID => session_id.to_i,
         BROWSER => browser.upcase,
         TIME => time.to_i,
         DATE => parse_date(date),
@@ -79,6 +81,12 @@ class ParserOptimized
     DATES = 'dates'.freeze
 
     MIN_POSTFIX = ' min.'
+
+    CHROME = 'CHROME'
+
+    INTERNET_EXPLORER = 'INTERNET EXPLORER'
+
+    BROWSER_SEPARATOR = ', '
 
     def work(filename = 'data_large.txt')
       users = []
@@ -146,38 +154,24 @@ class ParserOptimized
       report[USER_STATS] = {}
 
       # Собираем количество сессий по пользователям
-      collect_stats_from_users(report, users_objects) do |user|
-        { SESSION_COUNT => user.sessions.count }
-      end
-
       # Собираем количество времени по пользователям
-      collect_stats_from_users(report, users_objects) do |user|
-        { TOTAL_TIME => "#{user.sessions.map {|s| s[TIME] }.sum}#{MIN_POSTFIX}" }
-      end
-
       # Выбираем самую длинную сессию пользователя
-      collect_stats_from_users(report, users_objects) do |user|
-        { LONGEST_SESSION => "#{user.sessions.map {|s| s[TIME] }.max}#{MIN_POSTFIX}" }
-      end
-
       # Браузеры пользователя через запятую
-      collect_stats_from_users(report, users_objects) do |user|
-        { BROWSERS => user.sessions.map {|b| b[BROWSER]}.sort.join(', ') }
-      end
-
       # Хоть раз использовал IE?
-      collect_stats_from_users(report, users_objects) do |user|
-        { USED_IE => user.sessions.map {|b| b[BROWSER]}.any? { |b| b =~ /INTERNET EXPLORER/ } }
-      end
-
       # Всегда использовал только Chrome?
-      collect_stats_from_users(report, users_objects) do |user|
-        { ALWAYS_USED_CHROME => user.sessions.map {|b| b[BROWSER]}.all? { |b| b =~ /CHROME/ } }
-      end
-
       # Даты сессий через запятую в обратном порядке в формате iso8601
       collect_stats_from_users(report, users_objects) do |user|
-        { DATES => user.sessions.map{ |s| s[DATE] }.sort.reverse }
+        sessions_times = user.sessions.map {|s| s[TIME] }
+        browsers = user.sessions.map { |b| b[BROWSER] }
+        {
+          SESSION_COUNT => user.sessions.count,
+          TOTAL_TIME => "#{sessions_times.sum}#{MIN_POSTFIX}",
+          LONGEST_SESSION => "#{sessions_times.max}#{MIN_POSTFIX}",
+          BROWSERS => browsers.sort.join(BROWSER_SEPARATOR),
+          USED_IE => browsers.any? { |b| b.start_with? INTERNET_EXPLORER },
+          ALWAYS_USED_CHROME => browsers.all? { |b| b.start_with? CHROME },
+          DATES => user.sessions.map{ |s| s[DATE] }.sort.reverse
+        }
       end
 
       File.write('result.json', "#{report.to_json}\n")

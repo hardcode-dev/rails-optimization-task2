845642e594e36b764859e200d309eef1675a58a5
avbelyshev <avbelishev@gmail.com>
2021-05-10 16:30:33 +0300

    Add profilers

--- profilers/stackprof.rb
+++ profilers/stackprof.rb
@@ -14,7 +14,12 @@ require_relative '../src/report'
 
 # Note mode: :object
 GC.disable
-StackProf.run(mode: :object, out: '../reports/stackprof.dump', raw: true) do
-  work('../data_16000.txt', disable_gc: false)
+# StackProf.run(mode: :object, out: '../reports/stackprof.dump', raw: true) do
+#   work('../data_16000.txt')
+# end
+
+profile = StackProf.run(mode: :object, raw: true) do
+  work('../data_16000.txt')
 end
+File.write('../reports/stackprof.json', JSON.generate(profile))
 GC.enable

baa3d0fccce2800758a0058b0982c21cf04a8aff
iL53n <ilsegribko@gmail.com>
2021-05-12 12:58:51 +0300

    feat(test): add memory perfomance spec

---
+++ spec/memory_perfomance_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../parser.rb'
+
+describe 'Memory performance' do
+  describe 'parsing' do
+    let(:metric_budget) { (79) * 1024 }
+
+    describe '_protection of the metric from further degradation' do
+      it '(expectations on the memory size(bytes))' do
+        expect { Parser.new.work('spec/test_data/data.txt') }
+          .to perform_allocation(metric_budget).bytes
+      end
+    end
+  end
+end

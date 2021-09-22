6bdb6e961907b816536bf16ef058d6d59db5f14d
Andrii Boreiko <andriyboreyko@gmail.com>
2021-09-22 22:21:19 +0300

    step 2 for improving performance

---
+++ ruby_prof_service.rb
@@ -0,0 +1,24 @@
+require 'ruby-prof'
+require_relative 'task-2'
+
+RubyProf.measure_mode = RubyProf::MEMORY
+# RubyProf.measure_mode = RubyProf::ALLOCATIONS
+
+result = RubyProf.profile do
+  work(filename: 'files/data_10_000.txt', disable_gc: true)
+end
+
+printer = RubyProf::FlatPrinter.new(result)
+printer.print(File.open("ruby_prof_reports/flat_#{Time.now.to_i}.txt", "w+"))
+
+printer_2 = RubyProf::GraphHtmlPrinter.new(result)
+printer_2.print(File.open("ruby_prof_reports/graph_#{Time.now.to_i}.html", "w+"))
+
+printer_3 = RubyProf::CallTreePrinter.new(result)
+printer_3.print(:path => "ruby_prof_reports", :profile => 'callgrind')
+
+printer_4 = RubyProf::DotPrinter.new(result)
+printer_4.print(File.open("ruby_prof_reports/graphviz_#{Time.now.to_i}.dot", 'w+'))
+
+printer_5 = RubyProf::CallStackPrinter.new(result)
+printer_5.print(File.open("ruby_prof_reports/callstack_#{Time.now.to_i}.html", 'w+'))
--- task-2.rb
+++ task-2.rb
@@ -14,9 +14,8 @@ class User
   end
 end
 
-def parse_user(user)
-  fields = user.split(',')
-  parsed_result = {
+def parse_user(fields)
+  {
     'id' => fields[1],
     'first_name' => fields[2],
     'last_name' => fields[3],
@@ -24,9 +23,8 @@ def parse_user(user)
   }
 end
 
-def parse_session(session)
-  fields = session.split(',')
-  parsed_result = {
+def parse_session(fields)
+  {
     'user_id' => fields[1],
     'session_id' => fields[2],
     'browser' => fields[3],

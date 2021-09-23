c514d8ae6b3f878f7d081e47d3b22b8b266cb8f7
ordm <ordm67@gmail.com>
2021-09-23 16:11:41 +0300

    init

---
+++ Gemfile
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'date'
+gem 'json'
+
+# Debug
+gem 'pry'
+
+# Profiling
+gem 'memory_profiler'
+gem 'ruby-prof'
+gem 'stackprof'
+
+# Tests
+gem 'rspec'
---
+++ spec/task_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative '../task-2'
+
+RSpec.describe 'Task №2' do
+  describe '#work' do
+    let(:size) { 18 }
+
+    context 'health check' do
+      let(:result_data) { File.read('spec/fixtures/result.json') }
+
+      it 'returns users data(in json)' do
+        work('spec/fixtures/data18.txt')
+        expect(File.read('result.json')).to eq(result_data)
+      end
+    end
+  end
+end
+

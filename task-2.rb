927fc0a855c28195ad37b89d9c3e26253782bf02
Kirill Usanov <kirill.usanov.dev@gmail.com>
2021-09-27 01:04:30 +0300

    Refactoring

--- task-2.rb
+++ task-2.rb
@@ -4,47 +4,104 @@ require_relative 'user'
 require 'json'
 require 'date'
 
+IE_REGEXP = /INTERNET EXPLORER/
+CHROME_REGEXP = /CHROME/
+USER = 'user'.freeze
+SESSION = 'session'.freeze
+
+# Отчёт в json
+#   - Сколько всего юзеров +
+#   - Сколько всего уникальных браузеров +
+#   - Сколько всего сессий +
+#   - Перечислить уникальные браузеры в алфавитном порядке через запятую и капсом +
+#
+#   - По каждому пользователю
+#     - сколько всего сессий +
+#     - сколько всего времени +
+#     - самая длинная сессия +
+#     - браузеры через запятую +
+#     - Хоть раз использовал IE? +
+#     - Всегда использовал только Хром? +
+#     - даты сессий в порядке убывания через запятую +
+
 def work(filename = 'data.txt')
-  users = collect_data(filename)
+  init_variables
+  collect_data(filename)
+  calculate_total_stats
+  File.write('result.json', "#{@report.to_json}\n")
+end
 
-  sessions = users.map(&:sessions).flatten
-  report = {}
-  report[:totalUsers] = users.size
-  report[:uniqueBrowsersCount] = sessions.map { |s| s[:browser] }.uniq.count
-  report[:totalSessions] = sessions.count
-  report[:allBrowsers] = sessions.map { |s| s[:browser] }.map { |b| b.upcase }.uniq.sort.join(',')
-  report[:usersStats] = {}
-
-  # Собираем количество сессий по пользователям
-  collect_stats_from_users(report, users) do |user|
-    {
-      sessionsCount: user.sessions.count,
-      totalTime: user.sessions.map { |s| s[:time] }.map {|t| t.to_i}.sum.to_s + ' min.',
-      longestSession: user.sessions.map { |s| s[:time] }.map {|t| t.to_i}.max.to_s + ' min.',
-      browsers: user.sessions.map { |s| s[:browser] }.map { |b| b.upcase }.sort.join(', '),
-      usedIE: user.sessions.map { |s| s[:browser] }.any? { |b| b.upcase =~ /INTERNET EXPLORER/ },
-      alwaysUsedChrome: user.sessions.map { |s| s[:browser] }.all? { |b| b.upcase =~ /CHROME/ },
-      dates: user.sessions.map { |s| s[:date] }.sort.reverse
-    }
-  end
-
-  File.write('result.json', "#{report.to_json}\n")
+def init_variables
+  @unique_browsers = []
+  @report = { usersStats: {} }
 end
 
 def collect_data(filename)
-  users = []
+  reset_user_stats
 
-  File.read(filename).each_line do |line|
-    cols = line.rstrip.split(',')
+  File.read(filename).each_line(chomp: true) do |line|
+    cols = line_to_ary(line)
     case cols[0]
-    when 'user'
-      users << User.new(attributes: parse_user(cols))
-    when 'session'
-      users.last.sessions << parse_session(cols)
+    when USER
+      calculate_current_user_stats if current_user
+      User.create(attributes: parse_user(cols))
+    when SESSION
+      session = parse_session(cols)
+      current_user.sessions << session
+      add_to_stats(session)
     end
   end
 
-  users
+  calculate_current_user_stats
+end
+
+def add_to_stats(session)
+  # For all users
+  @unique_browsers << session[:browser] unless @unique_browsers.include?(session[:browser])
+
+  # For current_user
+  upcased_browser = session[:browser].upcase
+  @user_total_time += session[:time]
+  @user_longest_session = session[:time] if @user_longest_session < session[:time]
+  @user_browsers << upcased_browser
+  @used_ie = true if upcased_browser.match?(IE_REGEXP)
+  @used_only_chrome = false unless upcased_browser.match?(CHROME_REGEXP)
+end
+
+def calculate_total_stats
+  @report[:totalUsers] = User.size
+  @report[:uniqueBrowsersCount] = @unique_browsers.size
+  @report[:totalSessions] = User.sum { |user| user.sessions.size }
+  @report[:allBrowsers] = @unique_browsers.map(&:upcase).sort.join(',')
+end
+
+def calculate_current_user_stats
+  @report[:usersStats][current_user.full_name] = current_user_stats
+  reset_user_stats
+end
+
+def reset_user_stats
+  @user_total_time = 0
+  @user_longest_session = 0
+  @user_browsers = []
+  @used_ie = false
+  @used_only_chrome = true
+end
+
+def current_user_stats
+  {
+    sessionsCount: current_user.sessions.size,
+    totalTime: "#{@user_total_time} min.",
+    longestSession: "#{@user_longest_session} min.",
+    browsers: @user_browsers.sort.join(', '),
+    usedIE: @used_ie,
+    alwaysUsedChrome: @used_only_chrome,
+    dates: current_user.sessions.map { |s| s[:date] }.sort.reverse
+  }
+end
+
+def current_user
+  User.last
 end
 
 def parse_user(fields)
@@ -61,14 +118,11 @@ def parse_session(fields)
     user_id: fields[1],
     session_id: fields[2],
     browser: fields[3],
-    time: fields[4],
+    time: fields[4].to_i,
     date: fields[5]
   }
 end
 
-def collect_stats_from_users(report, users_objects, &block)
-  users_objects.each do |user|
-    user_key = "#{user.attributes[:first_name]} #{user.attributes[:last_name]}"
-    report[:usersStats][user_key] = block.call(user)
-  end
+def line_to_ary(line)
+  line.split(',')
 end
--- user.rb
+++ user.rb
@@ -1,11 +1,34 @@
 # frozen_string_literal: true
 
+require 'forwardable'
+
 class User
   attr_reader :attributes
   attr_accessor :sessions
+
+  # It will be great to use ObjectSpace.each_object(User) in this example instead of defining class attribute,
+  # but it can lead to unpredictable results if GC decides to delete User instances.
+  @all = []
+
+  class << self
+    extend Forwardable
+
+    attr_reader :all
+
+    def_delegators :all, :sum, :size, :last
+
+    def create(attributes:)
+      @all << new(attributes: attributes)
+    end
+  end
 
   def initialize(attributes:)
     @attributes = attributes
     @sessions = []
   end
+
+  def full_name
+    @full_name ||= "#{attributes[:first_name]} #{attributes[:last_name]}"
+  end
+
 end

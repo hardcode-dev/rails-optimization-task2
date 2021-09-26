c47fe8201a3e783f69621a93bf707ba21dafcc2f
Kirill Usanov <kirill.usanov.dev@gmail.com>
2021-09-26 14:49:50 +0300

    Initial

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+gem 'benchmark'
+gem 'memory_profiler'
+gem 'rubocop-performance', require: false
+gem 'ruby-prof'
+gem 'stackprof'
---
+++ profiler2.rb
@@ -0,0 +1,20 @@
+require 'ruby-prof'
+require_relative 'task-2'
+
+RubyProf.measure_mode = RubyProf::ALLOCATIONS
+
+result = RubyProf.profile do
+  work('data20000.txt')
+end
+
+printer = RubyProf::FlatPrinter.new(result)
+printer.print(File.open('reports/flat.txt', 'w+'))
+
+printer = RubyProf::DotPrinter.new(result)
+printer.print(File.open('reports/graphviz.dot', 'w+'))
+
+printer = RubyProf::GraphHtmlPrinter.new(result)
+printer.print(File.open('reports/graph.html', 'w+'))
+
+printer = RubyProf::CallStackPrinter.new(result)
+printer.print(File.open('reports/callstack.html', 'w+'))

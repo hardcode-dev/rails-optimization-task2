4d4aa853085e260723cf7eaa9e7ccd91520f76f1
sikorsky <alexandr.sikorsky@appodeal.com>
2021-10-23 19:48:44 +0300

    use Oj::Streamwriter

--- task-2.rb
+++ task-2.rb
@@ -4,6 +4,7 @@ require 'json'
 require 'pry'
 require 'date'
 require 'set'
+require 'oj'
 
 USER_COLUMNS = %w[_ id first_name last_name age].freeze
 SESSION_COLUMNS = %w[_ user_id session_id browser time date].freeze
@@ -52,29 +53,39 @@ def mem_usage
   `ps -o rss= -p #{Process.pid}`.to_i / 1024
 end
 
+def initialize_stream_writer
+  @file = File.open('result.json', 'w')
+  @stream_writer = Oj::StreamWriter.new(@file)
+  @stream_writer.push_object
+end
+
 def add_user_stat(user, user_sessions, report)
   user = User.new(attributes: user, sessions: user_sessions)
   user_key = "#{user.attributes['first_name']}" + ' ' + "#{user.attributes['last_name']}"
 
-  report['usersStats'][user_key] ||= {}
-  report['usersStats'][user_key] = report['usersStats'][user_key]
-                                     .merge('sessionsCount' => user.sessions.count)
-                                     .merge('totalTime' => user.sessions.map {|s| s['time']}.map {|t| t.to_i}.sum.to_s + ' min.')
-                                     .merge('longestSession' => user.sessions.map {|s| s['time']}.map {|t| t.to_i}.max.to_s + ' min.')
-                                     .merge('browsers' => user.sessions.map {|s| s['browser'] }.sort.join(', '))
-                                     .merge('usedIE' => user.sessions.map{|s| s['browser']}.any? { |b| b =~ /INTERNET EXPLORER/ })
-                                     .merge('alwaysUsedChrome' => user.sessions.map{|s| s['browser']}.all? { |b| b =~ /CHROME/ })
-                                     .merge('dates' => user.sessions.map{ |s| s['date'].chomp }.sort.reverse)
+  @stream_writer.push_key(user_key)
+  @stream_writer.push_value(
+    'sessionsCount' => user.sessions.count,
+    'totalTime' => user.sessions.map {|s| s['time']}.map {|t| t.to_i}.sum.to_s + ' min.',
+    'longestSession' => user.sessions.map {|s| s['time']}.map {|t| t.to_i}.max.to_s + ' min.',
+    'browsers' => user.sessions.map {|s| s['browser'] }.sort.join(', '),
+    'usedIE' => user.sessions.map{|s| s['browser']}.any? { |b| b =~ /INTERNET EXPLORER/ },
+    'alwaysUsedChrome' => user.sessions.map{|s| s['browser']}.all? { |b| b =~ /CHROME/ },
+    'dates' => user.sessions.map{ |s| s['date'].chomp }.sort.reverse
+  )
 end
 
 def work(file_name: 'data.txt')
+  initialize_stream_writer
   report = {}
   report[:totalUsers] = 0
-  report['totalSessions'] = 0
-  report['usersStats'] = {}
+  report[:totalSessions] = 0
   all_browsers = []
   current_user = nil
   current_user_sessions = nil
+
+  @stream_writer.push_key('usersStats')
+  @stream_writer.push_object
   File.new(file_name).each_line do |line|
     case line[0]
     when 'u'
@@ -85,15 +96,23 @@ def work(file_name: 'data.txt')
     when 's'
       session = parse_session(line)
       current_user_sessions << session
-      report['totalSessions'] += 1
+      report[:totalSessions] += 1
       all_browsers << session['browser']
     end
   end
   add_user_stat(current_user, current_user_sessions, report)
+  @stream_writer.pop
+
   uniq_browsers = all_browsers.uniq
   report['uniqueBrowsersCount'] = uniq_browsers.count
   report['allBrowsers'] = uniq_browsers.uniq.sort.join(',')
 
-  File.write('result.json', "#{report.to_json}\n")
+  report.each do |k, v|
+    @stream_writer.push_key(k.to_s)
+    @stream_writer.push_value(v)
+  end
+  @stream_writer.pop_all
   puts "MEMORY USAGE: %d MB" % (`ps -o rss= -p #{Process.pid}`.to_i / 1024)
+ensure
+  @file.close
 end

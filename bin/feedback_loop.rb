1fdbd5166cc9869166a03953234b2a3a40fd516b
sikorsky <alexandr.sikorsky@appodeal.com>
2021-10-25 00:04:21 +0300

    fix styling

--- bin/feedback_loop.rb
+++ bin/feedback_loop.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'benchmark'
 require 'memory_profiler'
 require 'stackprof'
@@ -7,17 +8,17 @@ require 'ruby-prof'
 require_relative '../task-2'
 
 def prepare_to_profile(gc_disable: true, dataset_size: 600_000)
-	`head -n #{dataset_size} data_large.txt > data.txt`
-	# `cp data_large.txt data.txt`
-	GC.disable if gc_disable
+  `head -n #{dataset_size} data_large.txt > data.txt`
+  # `cp data_large.txt data.txt`
+  GC.disable if gc_disable
 end
 
 def memory_profiler_report
-	prepare_to_profile(gc_disable: false)
-	report = MemoryProfiler.report do
-		work(file_name: 'data.txt')
-	end
-	report.pretty_print(scale_bytes: true)
+  prepare_to_profile(gc_disable: false)
+  report = MemoryProfiler.report do
+    work(file_name: 'data.txt')
+  end
+  report.pretty_print(scale_bytes: true)
 end
 
 # NOTE: second column represents own memory consumption (without callees)
@@ -28,40 +29,40 @@ end
 # stackprof --graphviz tmp/reports/stackprof_reports/stackprof.dump > graphviz.dot
 # dot -T png graphviz.dot > graphviz.png
 def stackprof_profiler_dump
-	prepare_to_profile(gc_disable: true)
-	StackProf.run(mode: :object, out: 'tmp/reports/stackprof_reports/stackprof.dump', raw: true) do
-		work(file_name: 'data.txt')
-	end
+  prepare_to_profile(gc_disable: true)
+  StackProf.run(mode: :object, out: 'tmp/reports/stackprof_reports/stackprof.dump', raw: true) do
+    work(file_name: 'data.txt')
+  end
 end
 
 def rubyprof_profiler(mode: 'memory')
-	RubyProf.measure_mode = mode == 'memory' ? RubyProf::MEMORY : RubyProf::ALLOCATIONS
-	prepare_to_profile(gc_disable: true)
-	result = RubyProf.profile { work }
+  RubyProf.measure_mode = mode == 'memory' ? RubyProf::MEMORY : RubyProf::ALLOCATIONS
+  prepare_to_profile(gc_disable: true)
+  result = RubyProf.profile { work }
 
-	RubyProf::FlatPrinter.new(result).print(STDOUT)
-	RubyProf::DotPrinter.new(result).print(File.open('tmp/reports/rubyprof/graphviz.dot', 'w+'))
-	# open tmp/reports/rubyprof/graph.html
-	RubyProf::GraphHtmlPrinter.new(result).print(File.open('tmp/reports/rubyprof/graph.html', 'w+'))
-	# open tmp/reports/rubyprof/callstack.html
-	RubyProf::CallStackPrinter.new(result).print(File.open('tmp/reports/rubyprof/callstack.html', 'w+'))
-	# qcachegrind tmp/reports/rubyprof/profile.
-	RubyProf::CallTreePrinter.new(result).print(path: 'tmp/reports/rubyprof', profile: 'profile')
+  RubyProf::FlatPrinter.new(result).print(STDOUT)
+  RubyProf::DotPrinter.new(result).print(File.open('tmp/reports/rubyprof/graphviz.dot', 'w+'))
+  # open tmp/reports/rubyprof/graph.html
+  RubyProf::GraphHtmlPrinter.new(result).print(File.open('tmp/reports/rubyprof/graph.html', 'w+'))
+  # open tmp/reports/rubyprof/callstack.html
+  RubyProf::CallStackPrinter.new(result).print(File.open('tmp/reports/rubyprof/callstack.html', 'w+'))
+  # qcachegrind tmp/reports/rubyprof/profile.
+  RubyProf::CallTreePrinter.new(result).print(path: 'tmp/reports/rubyprof', profile: 'profile')
 end
 
-def asymptotic_analysis(steps: 10, multiplicator: 2, start_amount: 10000)
-	amount = start_amount
-	steps.times do
-		puts `bundle exec ruby bin/runner.rb #{amount}`
-		puts '-------------'
-		amount *= multiplicator
-	end
+def asymptotic_analysis(steps: 10, multiplicator: 2, start_amount: 10_000)
+  amount = start_amount
+  steps.times do
+    puts `bundle exec ruby bin/runner.rb #{amount}`
+    puts '-------------'
+    amount *= multiplicator
+  end
 end
 
 def run
-	prepare_to_profile(gc_disable: false)
-	puts "TIME: #{Benchmark.realtime { work } }"
-	puts GC.stat
+  prepare_to_profile(gc_disable: false)
+  puts "TIME: #{Benchmark.realtime { work }}"
+  puts GC.stat
 end
 
 # def show_object_space_stat

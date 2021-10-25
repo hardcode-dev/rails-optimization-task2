4b4039e882f9cc28b0ce2fff8d612f742cb0e263
alex707 <alex.grigorenko2942@gmail.com>
2021-10-25 11:06:38 +0300

    Update task-2.rb

--- task-2.rb
+++ task-2.rb
@@ -1,9 +1,13 @@
-# Deoptimized version of homework task
+# 1
 
 require 'json'
 require 'pry'
 require 'date'
-require 'minitest/autorun'
+require 'minitest'
+require 'minitest/autorun' if ENV['RACK_ENV'] == 'test'
+require 'minitest/benchmark' if ENV['RACK_ENV'] == 'test'
+require 'ruby-prof' if ENV['RACK_ENV'] == 'benchmark'
+require 'stackprof' if ENV['RACK_ENV'] == 'benchmark'
 
 class User
   attr_reader :attributes, :sessions
@@ -14,8 +18,7 @@ class User
   end
 end
 
-def parse_user(user)
-  fields = user.split(',')
+def parse_user(fields)
   parsed_result = {
     'id' => fields[1],
     'first_name' => fields[2],
@@ -24,8 +27,7 @@ def parse_user(user)
   }
 end
 
-def parse_session(session)
-  fields = session.split(',')
+def parse_session(fields)
   parsed_result = {
     'user_id' => fields[1],
     'session_id' => fields[2],
@@ -44,15 +46,17 @@ def collect_stats_from_users(report, users_objects, &block)
 end
 
 def work
-  file_lines = File.read('data.txt').split("\n")
+  file_name = 'dataN.txt'
+  file_name = 'data.txt' if ENV['RACK_ENV'] == 'test'
+  file_lines = File.read(file_name).split("\n")
 
   users = []
   sessions = []
 
   file_lines.each do |line|
     cols = line.split(',')
-    users = users + [parse_user(line)] if cols[0] == 'user'
-    sessions = sessions + [parse_session(line)] if cols[0] == 'session'
+    users = users + [parse_user(cols)] if cols[0] == 'user'
+    sessions = sessions + [parse_session(cols)] if cols[0] == 'session'
   end
 
   # Отчёт в json
@@ -144,10 +148,11 @@ def work
   puts "MEMORY USAGE: %d MB" % (`ps -o rss= -p #{Process.pid}`.to_i / 1024)
 end
 
-class TestMe < Minitest::Test
-  def setup
-    File.write('result.json', '')
-    File.write('data.txt',
+if ENV['RACK_ENV'] == 'test'
+  class TestMe < Minitest::Test
+    def setup
+      File.write('result.json', '')
+      File.write('data.txt',
 'user,0,Leida,Cira,0
 session,0,0,Safari 29,87,2016-10-23
 session,0,1,Firefox 12,118,2017-02-27
@@ -167,11 +172,59 @@ session,2,1,Safari 49,85,2017-05-22
 session,2,2,Firefox 47,17,2018-02-02
 session,2,3,Chrome 20,84,2016-11-25
 ')
+    end
+
+    def test_result
+      work
+      expected_result = JSON.parse('{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}')
+      assert_equal expected_result, JSON.parse(File.read('result.json'))
+    end
+  end
+elsif ENV['RACK_ENV'] == 'benchmark'
+  # report = MemoryProfiler.report do
+  #   work
+  # end
+  # report.pretty_print(scale_bytes: true)
+
+
+  RubyProf.measure_mode = RubyProf::ALLOCATIONS
+  result = RubyProf.profile do
+    work
   end
 
-  def test_result
-    work
-    expected_result = JSON.parse('{"totalUsers":3,"uniqueBrowsersCount":14,"totalSessions":15,"allBrowsers":"CHROME 13,CHROME 20,CHROME 35,CHROME 6,FIREFOX 12,FIREFOX 32,FIREFOX 47,INTERNET EXPLORER 10,INTERNET EXPLORER 28,INTERNET EXPLORER 35,SAFARI 17,SAFARI 29,SAFARI 39,SAFARI 49","usersStats":{"Leida Cira":{"sessionsCount":6,"totalTime":"455 min.","longestSession":"118 min.","browsers":"FIREFOX 12, INTERNET EXPLORER 28, INTERNET EXPLORER 28, INTERNET EXPLORER 35, SAFARI 29, SAFARI 39","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-09-27","2017-03-28","2017-02-27","2016-10-23","2016-09-15","2016-09-01"]},"Palmer Katrina":{"sessionsCount":5,"totalTime":"218 min.","longestSession":"116 min.","browsers":"CHROME 13, CHROME 6, FIREFOX 32, INTERNET EXPLORER 10, SAFARI 17","usedIE":true,"alwaysUsedChrome":false,"dates":["2017-04-29","2016-12-28","2016-12-20","2016-11-11","2016-10-21"]},"Gregory Santos":{"sessionsCount":4,"totalTime":"192 min.","longestSession":"85 min.","browsers":"CHROME 20, CHROME 35, FIREFOX 47, SAFARI 49","usedIE":false,"alwaysUsedChrome":false,"dates":["2018-09-21","2018-02-02","2017-05-22","2016-11-25"]}}}')
-    assert_equal expected_result, JSON.parse(File.read('result.json'))
-  end
+  printer = RubyProf::FlatPrinter.new(result)
+  printer.print(File.open('ruby_prof_reports/flat.txt', 'w+'))
+
+  # printer = RubyProf::DotPrinter.new(result)
+  # printer.print(File.open('ruby_prof_reports/graphviz.dot', 'w+'))
+
+  # printer = RubyProf::GraphHtmlPrinter.new(result)
+  # printer.print(File.open('ruby_prof_reports/graph.html', 'w+'))
+
+  # printer = RubyProf::CallStackPrinter.new(result)
+  # printer.print(File.open('ruby_prof_reports/callstack.html', 'w+'))
+
+
+
+  # RubyProf.measure_mode = RubyProf::MEMORY
+
+  # result = RubyProf.profile do
+  #   work
+  # end
+
+  # printer = RubyProf::CallTreePrinter.new(result)
+  # printer.print(path: 'ruby_prof_reports', profile: 'profile')
+
+
+
+  # StackProf.run(mode: :object, out: 'stackprof_reports/stackprof.dump', raw: true) do
+  #   work
+  # end
+
+  # profile = StackProf.run(mode: :object, raw: true) do
+  #   work
+  # end
+  # File.write('stackprof_reports/stackprof.json', JSON.generate(profile))
+else
+  work
 end

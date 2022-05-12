da2829df0dcdddf0af2b5babaa005cd278ea3c0a
Aleksandr Kariakin <aleksandr.kariakin@toptal.com>
2022-05-12 17:33:43 +0300

    Finish squash

---
+++ instruments/ruby_prof.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'ruby-prof'
+require_relative '../task-2'
+
+RubyProf.measure_mode = RubyProf::ALLOCATIONS
+
+result = RubyProf.profile do
+  work
+end
+
+printer = RubyProf::FlatPrinter.new(result)
+printer.print(File.open('instruments/ruby_prof_reports/flat.txt', 'w+'))
+
+printer = RubyProf::GraphHtmlPrinter.new(result)
+printer.print(File.open('instruments/ruby_prof_reports/graph.html', 'w+'))
+
+printer = RubyProf::CallStackPrinter.new(result)
+printer.print(File.open('instruments/ruby_prof_reports/callstack.html', 'w+'))

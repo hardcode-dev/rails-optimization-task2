88c9d6ac2e3f1b265777838ccb99cc024a030d1b
Efremov Evgenii <e.efremov@3commas.io>
2022-05-31 21:00:09 +0300

    add test file

--- performance_spec.rb
+++ performance_spec.rb
@@ -8,7 +8,7 @@ end
 describe 'Performance' do
   let(:file_name) { '10_000.txt' }
 
-  it 'На обработку выделяется не более 15 MB памяти' do
+  it 'На обработку выделяется не более 10 MB памяти' do
     expect do
       work(file_name: file_name)
     end.to perform_allocation(15 * 1024 * 1024).memory

009bfaf1512bd544dda44dcc7834462dd950ef51
Nikita Plyusnin <nplyusnin78@gmail.com>
2023-03-10 22:14:38 +0300

    memory profiler

--- task-2.rb
+++ task-2.rb
@@ -1,3 +1,7 @@
 require_relative "./lib/user.rb"
+require 'memory_profiler'
 
-work(input_path: "./data/data_sample.txt", output_path: "./result.json")
+report = MemoryProfiler.report do
+  work(input_path: "./data/data_sample.txt", output_path: "./result.json")
+end
+report.pretty_print(scale_bytes: true)

963a2bc3e9aec424429574a937fa2e5f37ed10cb
Nikita Plyusnin <nplyusnin78@gmail.com>
2023-03-10 22:58:23 +0300

    add profilers

--- task-2.rb
+++ task-2.rb
@@ -1,7 +1,37 @@
 require_relative "./lib/user.rb"
 require 'memory_profiler'
+require 'stackprof'
+require 'ruby-prof'
 
-report = MemoryProfiler.report do
-  work(input_path: "./data/data_sample.txt", output_path: "./result.json")
-end
-report.pretty_print(scale_bytes: true)
+# report = MemoryProfiler.report do
+#   work(input_path: "./data/data_sample.txt", output_path: "./result.json")
+# end
+
+# report.pretty_print(scale_bytes: true)
+
+# StackProf.run(mode: :object, out: 'reports/stackprof_memory.dump', raw: true) do
+#   work(input_path: "./data/data_sample.txt", output_path: "./result.json")
+# end
+
+# RubyProf.measure_mode = RubyProf::MEMORY
+
+# result = RubyProf.profile do
+#   work(input_path: './data/data_sample.txt', output_path: './result.json')
+# end
+
+# printer = RubyProf::FlatPrinter.new(result)
+# printer.print(File.open("./reports/memory_flat.txt", "w+"))
+
+# printer = RubyProf::GraphHtmlPrinter.new(result)
+# printer.print(File.open("./reports/memory_graph.html", "w+"))
+
+# printer = RubyProf::CallStackPrinter.new(result)
+# printer.print(File.open("./reports/memory_callstack.html", "w+"))
+
+# printer = RubyProf::CallTreePrinter.new(result)
+# printer.print(path: './reports/', profile: 'profile')
+
+work(input_path: './data/data_sample.txt', output_path: './result.json')
+
+# Allocated memory
+# Iteration 0: 3.78 GB

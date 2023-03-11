077daf3d52e73155abc94503c96c93c3bb068dc5
Tab Loid <tabloidmeister@gmail.com>
2023-03-12 02:56:44 +0300

    Remove unnecessary memory allocations for user object collection

--- task-2.rb
+++ task-2.rb
@@ -99,7 +99,7 @@ def work(file_name = 'data.txt')
     attributes = user
     user_sessions = sessions.select { |session| session['user_id'] == user['id'] }
     user_object = User.new(attributes: attributes, sessions: user_sessions)
-    users_objects = users_objects + [user_object]
+    users_objects << user_object
   end
 
   report['usersStats'] = {}

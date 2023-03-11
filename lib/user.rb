b05b195bfc05391033b4181c83636549a16930b8
Nikita Plyusnin <nplyusnin78@gmail.com>
2023-03-11 09:40:33 +0300

    iteration 3

--- lib/user.rb
+++ lib/user.rb
@@ -178,7 +178,7 @@ def work(input_path:, output_path:)
 
   # Даты сессий через запятую в обратном порядке в формате iso8601
   collect_stats_from_users(report, stat) do |user|
-    { 'dates' => user[:sessions].map{|s| s['date']}.map {|d| Date.parse(d)}.sort.reverse.map { |d| d.iso8601 } }
+    { 'dates' => user[:sessions].map{|s| s['date']}.sort.reverse }
   end
 
   File.write(output_path, "#{report.to_json}\n")

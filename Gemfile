85fe27a54434087192223237a602f98d587eeb88
Паращевин Григорий Андреевич <gparaschevin@stsoft.ru>
2023-03-18 20:26:14 +0300

    second step

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '3.2.1'
 
+gem 'set'
+
 # Debug
 gem 'pry'
 
--- test/work_spec.rb
+++ test/work_spec.rb
@@ -7,7 +7,15 @@ RSpec.configure do |config|
 end
 
 describe 'work' do
-  it 'allocates less than 131,000 bytes in memory  ' do
-    expect { work('data/data.txt') }.to perform_allocation(131000).bytes
+  it 'allocates less than n bytes in memory  ' do
+    expect { work('data/data_100000_lines.txt') }.to perform_allocation(131000).bytes
+  end
+end
+
+describe 'performance' do
+  it 'work n ms' do
+    expect {
+      work("data/data_20000_lines.txt")
+    }.to perform_under(1000).ms.warmup(2).times.sample(5).times
   end
 end

adaed8cfb56145fc0da5794a89fedf88089de208
Паращевин Григорий Андреевич <gparaschevin@stsoft.ru>
2023-03-18 19:36:48 +0300

    Initial preparation

---
+++ test/work_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec-benchmark'
+require 'rspec'
+require_relative '../task-2'
+
+RSpec.configure do |config|
+  config.include RSpec::Benchmark::Matchers
+end
+
+describe 'work' do
+  it 'allocates less than 131,000 bytes in memory  ' do
+    expect { work('data/data.txt') }.to perform_allocation(131000).bytes
+  end
+end

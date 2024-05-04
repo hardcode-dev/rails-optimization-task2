0d4089fd8bc86a14c9e8be0ac03c07c0abc2871f
Beglov Sergey <beglov.sergey@yandex.ru>
2024-05-04 13:30:25 +0300

    Setup ruby-prof

--- task-2.rb
+++ task-2.rb
@@ -4,6 +4,7 @@ require 'json'
 require 'pry'
 require 'date'
 require 'stackprof'
+require 'ruby-prof'
 require 'memory_profiler'
 require 'minitest/autorun'
 
@@ -204,6 +205,30 @@ end
 # report.pretty_print(scale_bytes: true)
 
 ### stackprof
-StackProf.run(mode: :object, out: 'stackprof_reports/stackprof.dump', raw: true) do
+# StackProf.run(mode: :object, out: 'stackprof_reports/stackprof.dump', raw: true) do
+#   work('data10000.txt')
+# end
+
+### ruby-prof
+# RubyProf.measure_mode = RubyProf::ALLOCATIONS
+# На этот раз профилируем не allocations, а объём памяти!
+RubyProf.measure_mode = RubyProf::MEMORY
+
+result = RubyProf.profile do
   work('data10000.txt')
 end
+
+printer = RubyProf::FlatPrinter.new(result)
+printer.print(File.open('ruby_prof_reports/flat.txt', 'w+'))
+
+# printer = RubyProf::DotPrinter.new(result)
+# printer.print(File.open('ruby_prof_reports/graphviz.dot', 'w+'))
+
+printer = RubyProf::GraphHtmlPrinter.new(result)
+printer.print(File.open('ruby_prof_reports/graph.html', 'w+'))
+
+printer = RubyProf::CallStackPrinter.new(result)
+printer.print(File.open('ruby_prof_reports/callstack.html', 'w+'))
+
+printer = RubyProf::CallTreePrinter.new(result)
+printer.print(path: 'ruby_prof_reports', profile: 'profile')

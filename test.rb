ed9520a09ddb6665edc01b85a35b1962172d3b71
Nikita Kononov <vocrsz@gmail.com>
2024-05-10 19:43:55 +0200

    attempt 1.1

--- test.rb
+++ test.rb
@@ -1,9 +1,20 @@
 #!/usr/bin/env ruby
 
 require_relative 'task-2'
-require 'memory_profiler'
 
-report = MemoryProfiler.report do
+# # ======== MEMORY PROFILER =========
+# require 'memory_profiler'
+
+# report = MemoryProfiler.report do
+#   work(File.join(__dir__, ARGV.first))
+# end
+# report.pretty_print(scale_bytes: true)
+# # ==================
+
+# ==================
+require 'stackprof'
+
+StackProf.run(mode: :object, out: 'stackprof_reports/stackprof.dump', raw: true) do
   work(File.join(__dir__, ARGV.first))
 end
-report.pretty_print(scale_bytes: true)
+# ==================

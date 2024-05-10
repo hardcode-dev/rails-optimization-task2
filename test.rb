a813ff298c92dfca442d96bf2ec8fe0ca6c2f8b3
Nikita Kononov <vocrsz@gmail.com>
2024-05-10 19:43:55 +0200

    attempt 1.2

--- test.rb
+++ test.rb
@@ -11,10 +11,32 @@ require_relative 'task-2'
 # report.pretty_print(scale_bytes: true)
 # # ==================
 
-# ==================
-require 'stackprof'
+# # ======== STACKPROF =========
+# require 'stackprof'
 
-StackProf.run(mode: :object, out: 'stackprof_reports/stackprof.dump', raw: true) do
+# StackProf.run(mode: :object, out: 'stackprof_reports/stackprof.dump', raw: true) do
+#   work(File.join(__dir__, ARGV.first))
+# end
+# # ==================
+
+# # ======== RUBY_PROF =========
+require 'ruby-prof'
+
+profiler = RubyProf::Profile.new(measure_mode: RubyProf::ALLOCATIONS, track_allocations: true)
+
+result = profiler.profile do
   work(File.join(__dir__, ARGV.first))
 end
-# ==================
+
+printer = RubyProf::FlatPrinter.new(result)
+printer.print(File.open('ruby_prof_reports/flat.txt', 'w+'))
+
+printer = RubyProf::DotPrinter.new(result)
+printer.print(File.open('ruby_prof_reports/graphviz.dot', 'w+'))
+
+printer = RubyProf::GraphHtmlPrinter.new(result)
+printer.print(File.open('ruby_prof_reports/graph.html', 'w+'))
+
+printer = RubyProf::CallStackPrinter.new(result)
+printer.print(File.open('ruby_prof_reports/callstack.html', 'w+'))
+# # ==================
